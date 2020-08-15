d89d4c4bcd6bb7d2182f694c8c018dd3e66ef275
Alok Ranjan <Alok.Ranjan@amwater.com>
2020-08-15 13:29:11 +0530

    Swagger Api Added

--- src/main/java/com/accionlabs/weatherApp/controllers/WeatherController.java
+++ src/main/java/com/accionlabs/weatherApp/controllers/WeatherController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import com.accionlabs.weatherApp.exception.WeatherException;
 import com.accionlabs.weatherApp.service.WeatherService;
 
 @RestController
+@RequestMapping(value="/weather")
 public class WeatherController {
 
 	@Autowired
--- src/main/java/com/accionlabs/weatherApp/helper/SwaggerConfig.java
+++ src/main/java/com/accionlabs/weatherApp/helper/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
-        		.select().apis(RequestHandlerSelectors.basePackage("com.codingTest.controllers"))
+        		.select().apis(RequestHandlerSelectors.basePackage("com.accionlabs.weatherApp.controllers"))
         		.paths(regex("/weather.*"))
                 .build();
     }
